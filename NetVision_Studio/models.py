0bda95bb75c32c26ef9558f00d7ed6bca5daaa97
FishyOfixial <ivanrdlt47@gmail.com>
2025-11-26 16:42:28 -0600

    Modelos

--- NetVision_Studio/models.py
+++ NetVision_Studio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Device(models.Model):
+    type = models.CharField(max_length=20, null=False, blank=False)
+    ip_device = models.GenericIPAddressField(unique=True)
+    hostname = models.CharField(max_length=50, blank=True, null=False, default='Device')
+
+class Interface(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='id_device')
+    name = models.CharField(max_length=20, null=False, blank=False)
+    state = models.BooleanField(default=False)
+    is_access = models.BooleanField(default=True)
+    native = models.PositiveIntegerField(default=1, null=False)
+    allowed_vlan = models.CharField(max_length=200, blank=True, null=True)
+
+class TopologyLink(models.Model):
+    device_a = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='link_a')
+    interface_a = models.ForeignKey(Interface, on_delete=models.CASCADE, related_name='int_a')
+    device_b = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='link_b')
+    interface_b = models.ForeignKey(Interface, on_delete=models.CASCADE, related_name='int_b')
+
+
