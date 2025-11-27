8b58885a4289fe6c97280023732f6a0c66c51f06
Juan Marco Gosselin Gamboa <juanmarcogosselingamboa@Juans-MacBook-Pro-3.local>
2025-11-27 13:09:58 -0600

    vistas para access y SWD html

--- NetVision_Studio/urls.py
+++ NetVision_Studio/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.multilayer_HTML, name = 'multilayer'),
+    path('acceso/', views.access_HTML, name='access')
+    
 ]
--- NetVision_Studio/views.py
+++ NetVision_Studio/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 
+def multilayer_HTML(request):
+    return render(request, "SWD.html")
+
+def access_HTML(request): 
+    return render(request, 'access.html')
